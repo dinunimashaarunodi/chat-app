e2f98a0457837d881f7e47e03720ac4402153cfd
dinunimasha <dinunimashaarunodi@gmail.com>
2025-07-18 20:58:01 +0530

    donewith the frontend

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -13,7 +13,21 @@ alt=''/>
       </div>
 
       <div className="chat-msg">
-        <div className="s-massage">
+        <div className="s-msg">
+          <p className='msg'>Lorem ipsum is placeholder text commonly used in..</p>
+          <div>
+            <img src={assets.profile_img} alt=''/>
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img  className='msg-img' src={assets.pic1} alt=''/>
+          <div>
+            <img src={assets.profile_img} alt=''/>
+            <p>2:30 PM</p>
+          </div>
+        </div>
+         <div className="r-msg">
           <p className='msg'>Lorem ipsum is placeholder text commonly used in..</p>
           <div>
             <img src={assets.profile_img} alt=''/>
